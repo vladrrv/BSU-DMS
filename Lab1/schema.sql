97b2c70d7b0c5ed484b7b6e7bef8761c16bc71bd
Vlad R <vladrrv@gmail.com>
2019-09-23 15:41:20 +0300

    lab 1

--- Lab1/schema.sql
+++ Lab1/schema.sql
@@ -1,4 +1,22 @@
 
+DROP TABLE Room CASCADE CONSTRAINTS PURGE;
+
+DROP TABLE Hotel CASCADE CONSTRAINTS PURGE;
+
+DROP TABLE Seat CASCADE CONSTRAINTS PURGE;
+
+DROP TABLE Flight CASCADE CONSTRAINTS PURGE;
+
+DROP TABLE City CASCADE CONSTRAINTS PURGE;
+
+DROP TABLE TourEnrollment CASCADE CONSTRAINTS PURGE;
+
+DROP TABLE Tour CASCADE CONSTRAINTS PURGE;
+
+DROP TABLE Country CASCADE CONSTRAINTS PURGE;
+
+DROP TABLE Customer CASCADE CONSTRAINTS PURGE;
+
 CREATE TABLE City
 (
     id                   INT NOT NULL ,
@@ -30,7 +48,7 @@ CREATE TABLE Customer
     id                   INT NOT NULL ,
     Name                 CHAR(18) NULL ,
     Address              CHAR(18) NULL ,
-    Passport             CHAR(18) NULL
+    Passport             CHAR(18) NULL CONSTRAINT PassportConstraint CHECK ( NOT REGEXP_LIKE(Passport,'^[[A-Z]{2}[0-9]{7}]$') )
 );
 
 CREATE UNIQUE INDEX XPKCustomer ON Customer
@@ -45,7 +63,7 @@ CREATE TABLE Flight
     City_id              INT NOT NULL ,
     Leave_Date           DATE NOT NULL ,
     Return_Date          DATE NOT NULL ,
-    CONSTRAINT FlightDateRule_1356924654 CHECK ( Leave_Date < Return_Date )
+    CONSTRAINT FlightDateConstraint CHECK ( Leave_Date < Return_Date )
 );
 
 CREATE UNIQUE INDEX XPKFlight ON Flight
@@ -162,14 +180,14 @@ ALTER TABLE TourEnrollment
 
 CREATE  TRIGGER  tD_City AFTER DELETE ON City for each row
     -- erwin Builtin Trigger
--- DELETE trigger on City 
+-- DELETE trigger on City
 DECLARE NUMROWS INTEGER;
 BEGIN
     /* erwin Builtin Trigger */
     /* City  Hotel on parent delete restrict */
     /* ERWIN_RELATION:CHECKSUM="00019b5e", PARENT_OWNER="", PARENT_TABLE="City"
     CHILD_OWNER="", CHILD_TABLE="Hotel"
-    P2C_VERB_PHRASE="", C2P_VERB_PHRASE="", 
+    P2C_VERB_PHRASE="", C2P_VERB_PHRASE="",
     FK_CONSTRAINT="R_9", FK_COLUMNS="City_id" */
     SELECT count(*) INTO NUMROWS
     FROM Hotel
@@ -188,7 +206,7 @@ BEGIN
     /* City  Flight on parent delete restrict */
     /* ERWIN_RELATION:CHECKSUM="00000000", PARENT_OWNER="", PARENT_TABLE="City"
     CHILD_OWNER="", CHILD_TABLE="Flight"
-    P2C_VERB_PHRASE="", C2P_VERB_PHRASE="", 
+    P2C_VERB_PHRASE="", C2P_VERB_PHRASE="",
     FK_CONSTRAINT="R_7", FK_COLUMNS="City_id" */
     SELECT count(*) INTO NUMROWS
     FROM Flight
@@ -210,14 +228,14 @@ END;
 
 CREATE  TRIGGER tI_City BEFORE INSERT ON City for each row
     -- erwin Builtin Trigger
--- INSERT trigger on City 
+-- INSERT trigger on City
 DECLARE NUMROWS INTEGER;
 BEGIN
     /* erwin Builtin Trigger */
     /* Country  City on child insert restrict */
     /* ERWIN_RELATION:CHECKSUM="0000e888", PARENT_OWNER="", PARENT_TABLE="Country"
     CHILD_OWNER="", CHILD_TABLE="City"
-    P2C_VERB_PHRASE="", C2P_VERB_PHRASE="", 
+    P2C_VERB_PHRASE="", C2P_VERB_PHRASE="",
     FK_CONSTRAINT="R_8", FK_COLUMNS="Country_id" */
     SELECT count(*) INTO NUMROWS
     FROM Country
@@ -243,7 +261,7 @@ END;
 
 CREATE  TRIGGER tU_City AFTER UPDATE ON City for each row
     -- erwin Builtin Trigger
--- UPDATE trigger on City 
+-- UPDATE trigger on City
 DECLARE NUMROWS INTEGER;
 BEGIN
     /* erwin Builtin Trigger */
@@ -325,14 +343,14 @@ END;
 
 CREATE  TRIGGER  tD_Country AFTER DELETE ON Country for each row
     -- erwin Builtin Trigger
--- DELETE trigger on Country 
+-- DELETE trigger on Country
 DECLARE NUMROWS INTEGER;
 BEGIN
     /* erwin Builtin Trigger */
     /* Country  Tour on parent delete restrict */
     /* ERWIN_RELATION:CHECKSUM="0001adff", PARENT_OWNER="", PARENT_TABLE="Country"
     CHILD_OWNER="", CHILD_TABLE="Tour"
-    P2C_VERB_PHRASE="", C2P_VERB_PHRASE="", 
+    P2C_VERB_PHRASE="", C2P_VERB_PHRASE="",
     FK_CONSTRAINT="R_16", FK_COLUMNS="Country_id" */
     SELECT count(*) INTO NUMROWS
     FROM Tour
@@ -351,7 +369,7 @@ BEGIN
     /* Country  City on parent delete restrict */
     /* ERWIN_RELATION:CHECKSUM="00000000", PARENT_OWNER="", PARENT_TABLE="Country"
     CHILD_OWNER="", CHILD_TABLE="City"
-    P2C_VERB_PHRASE="", C2P_VERB_PHRASE="", 
+    P2C_VERB_PHRASE="", C2P_VERB_PHRASE="",
     FK_CONSTRAINT="R_8", FK_COLUMNS="Country_id" */
     SELECT count(*) INTO NUMROWS
     FROM City
@@ -373,7 +391,7 @@ END;
 
 CREATE  TRIGGER tU_Country AFTER UPDATE ON Country for each row
     -- erwin Builtin Trigger
--- UPDATE trigger on Country 
+-- UPDATE trigger on Country
 DECLARE NUMROWS INTEGER;
 BEGIN
     /* erwin Builtin Trigger */
@@ -432,14 +450,14 @@ END;
 
 CREATE  TRIGGER  tD_Customer AFTER DELETE ON Customer for each row
     -- erwin Builtin Trigger
--- DELETE trigger on Customer 
+-- DELETE trigger on Customer
 DECLARE NUMROWS INTEGER;
 BEGIN
     /* erwin Builtin Trigger */
     /* Customer  TourEnrollment on parent delete restrict */
     /* ERWIN_RELATION:CHECKSUM="000264f3", PARENT_OWNER="", PARENT_TABLE="Customer"
     CHILD_OWNER="", CHILD_TABLE="TourEnrollment"
-    P2C_VERB_PHRASE="", C2P_VERB_PHRASE="", 
+    P2C_VERB_PHRASE="", C2P_VERB_PHRASE="",
     FK_CONSTRAINT="R_20", FK_COLUMNS="Customer_id" */
     SELECT count(*) INTO NUMROWS
     FROM TourEnrollment
@@ -458,7 +476,7 @@ BEGIN
     /* Customer Book Room on parent delete set null */
     /* ERWIN_RELATION:CHECKSUM="00000000", PARENT_OWNER="", PARENT_TABLE="Customer"
     CHILD_OWNER="", CHILD_TABLE="Room"
-    P2C_VERB_PHRASE="Book", C2P_VERB_PHRASE="", 
+    P2C_VERB_PHRASE="Book", C2P_VERB_PHRASE="",
     FK_CONSTRAINT="R_13", FK_COLUMNS="Customer_id" */
     UPDATE Room
     SET
@@ -472,7 +490,7 @@ BEGIN
     /* Customer Book Seat on parent delete set null */
     /* ERWIN_RELATION:CHECKSUM="00000000", PARENT_OWNER="", PARENT_TABLE="Customer"
     CHILD_OWNER="", CHILD_TABLE="Seat"
-    P2C_VERB_PHRASE="Book", C2P_VERB_PHRASE="", 
+    P2C_VERB_PHRASE="Book", C2P_VERB_PHRASE="",
     FK_CONSTRAINT="R_12", FK_COLUMNS="Customer_id" */
     UPDATE Seat
     SET
@@ -489,7 +507,7 @@ END;
 
 CREATE  TRIGGER tU_Customer AFTER UPDATE ON Customer for each row
     -- erwin Builtin Trigger
--- UPDATE trigger on Customer 
+-- UPDATE trigger on Customer
 DECLARE NUMROWS INTEGER;
 BEGIN
     /* erwin Builtin Trigger */
@@ -560,14 +578,14 @@ END;
 
 CREATE  TRIGGER  tD_Flight AFTER DELETE ON Flight for each row
     -- erwin Builtin Trigger
--- DELETE trigger on Flight 
+-- DELETE trigger on Flight
 DECLARE NUMROWS INTEGER;
 BEGIN
     /* erwin Builtin Trigger */
     /* Flight Have Seat on parent delete restrict */
     /* ERWIN_RELATION:CHECKSUM="0000cef8", PARENT_OWNER="", PARENT_TABLE="Flight"
     CHILD_OWNER="", CHILD_TABLE="Seat"
-    P2C_VERB_PHRASE="Have", C2P_VERB_PHRASE="", 
+    P2C_VERB_PHRASE="Have", C2P_VERB_PHRASE="",
     FK_CONSTRAINT="R_11", FK_COLUMNS="Flight_id" */
     SELECT count(*) INTO NUMROWS
     FROM Seat
@@ -589,14 +607,14 @@ END;
 
 CREATE  TRIGGER tI_Flight BEFORE INSERT ON Flight for each row
     -- erwin Builtin Trigger
--- INSERT trigger on Flight 
+-- INSERT trigger on Flight
 DECLARE NUMROWS INTEGER;
 BEGIN
     /* erwin Builtin Trigger */
     /* City  Flight on child insert restrict */
     /* ERWIN_RELATION:CHECKSUM="0000d8b3", PARENT_OWNER="", PARENT_TABLE="City"
     CHILD_OWNER="", CHILD_TABLE="Flight"
-    P2C_VERB_PHRASE="", C2P_VERB_PHRASE="", 
+    P2C_VERB_PHRASE="", C2P_VERB_PHRASE="",
     FK_CONSTRAINT="R_7", FK_COLUMNS="City_id" */
     SELECT count(*) INTO NUMROWS
     FROM City
@@ -622,7 +640,7 @@ END;
 
 CREATE  TRIGGER tU_Flight AFTER UPDATE ON Flight for each row
     -- erwin Builtin Trigger
--- UPDATE trigger on Flight 
+-- UPDATE trigger on Flight
 DECLARE NUMROWS INTEGER;
 BEGIN
     /* erwin Builtin Trigger */
@@ -680,14 +698,14 @@ END;
 
 CREATE  TRIGGER  tD_Hotel AFTER DELETE ON Hotel for each row
     -- erwin Builtin Trigger
--- DELETE trigger on Hotel 
+-- DELETE trigger on Hotel
 DECLARE NUMROWS INTEGER;
 BEGIN
     /* erwin Builtin Trigger */
     /* Hotel Have Room on parent delete restrict */
     /* ERWIN_RELATION:CHECKSUM="0000c4af", PARENT_OWNER="", PARENT_TABLE="Hotel"
     CHILD_OWNER="", CHILD_TABLE="Room"
-    P2C_VERB_PHRASE="Have", C2P_VERB_PHRASE="", 
+    P2C_VERB_PHRASE="Have", C2P_VERB_PHRASE="",
     FK_CONSTRAINT="R_10", FK_COLUMNS="Hotel_id" */
     SELECT count(*) INTO NUMROWS
     FROM Room
@@ -709,14 +727,14 @@ END;
 
 CREATE  TRIGGER tI_Hotel BEFORE INSERT ON Hotel for each row
     -- erwin Builtin Trigger
--- INSERT trigger on Hotel 
+-- INSERT trigger on Hotel
 DECLARE NUMROWS INTEGER;
 BEGIN
     /* erwin Builtin Trigger */
     /* City  Hotel on child insert restrict */
     /* ERWIN_RELATION:CHECKSUM="0000db28", PARENT_OWNER="", PARENT_TABLE="City"
     CHILD_OWNER="", CHILD_TABLE="Hotel"
-    P2C_VERB_PHRASE="", C2P_VERB_PHRASE="", 
+    P2C_VERB_PHRASE="", C2P_VERB_PHRASE="",
     FK_CONSTRAINT="R_9", FK_COLUMNS="City_id" */
     SELECT count(*) INTO NUMROWS
     FROM City
@@ -742,7 +760,7 @@ END;
 
 CREATE  TRIGGER tU_Hotel AFTER UPDATE ON Hotel for each row
     -- erwin Builtin Trigger
--- UPDATE trigger on Hotel 
+-- UPDATE trigger on Hotel
 DECLARE NUMROWS INTEGER;
 BEGIN
     /* erwin Builtin Trigger */
@@ -800,14 +818,14 @@ END;
 
 CREATE  TRIGGER tI_Room BEFORE INSERT ON Room for each row
     -- erwin Builtin Trigger
--- INSERT trigger on Room 
+-- INSERT trigger on Room
 DECLARE NUMROWS INTEGER;
 BEGIN
     /* erwin Builtin Trigger */
     /* Customer Book Room on child insert set null */
     /* ERWIN_RELATION:CHECKSUM="0001dec4", PARENT_OWNER="", PARENT_TABLE="Customer"
     CHILD_OWNER="", CHILD_TABLE="Room"
-    P2C_VERB_PHRASE="Book", C2P_VERB_PHRASE="", 
+    P2C_VERB_PHRASE="Book", C2P_VERB_PHRASE="",
     FK_CONSTRAINT="R_13", FK_COLUMNS="Customer_id" */
     UPDATE Room
     SET
@@ -828,7 +846,7 @@ BEGIN
     /* Hotel Have Room on child insert restrict */
     /* ERWIN_RELATION:CHECKSUM="00000000", PARENT_OWNER="", PARENT_TABLE="Hotel"
     CHILD_OWNER="", CHILD_TABLE="Room"
-    P2C_VERB_PHRASE="Have", C2P_VERB_PHRASE="", 
+    P2C_VERB_PHRASE="Have", C2P_VERB_PHRASE="",
     FK_CONSTRAINT="R_10", FK_COLUMNS="Hotel_id" */
     SELECT count(*) INTO NUMROWS
     FROM Hotel
@@ -854,14 +872,14 @@ END;
 
 CREATE  TRIGGER tU_Room AFTER UPDATE ON Room for each row
     -- erwin Builtin Trigger
--- UPDATE trigger on Room 
+-- UPDATE trigger on Room
 DECLARE NUMROWS INTEGER;
 BEGIN
     /* erwin Builtin Trigger */
     /* Customer Book Room on child update set null */
     /* ERWIN_RELATION:CHECKSUM="0001849c", PARENT_OWNER="", PARENT_TABLE="Customer"
     CHILD_OWNER="", CHILD_TABLE="Room"
-    P2C_VERB_PHRASE="Book", C2P_VERB_PHRASE="", 
+    P2C_VERB_PHRASE="Book", C2P_VERB_PHRASE="",
     FK_CONSTRAINT="R_13", FK_COLUMNS="Customer_id" */
     /* Not Implemented due to an ORA-04091 Mutating Table Issue */
     NULL;
@@ -897,14 +915,14 @@ END;
 
 CREATE  TRIGGER tI_Seat BEFORE INSERT ON Seat for each row
     -- erwin Builtin Trigger
--- INSERT trigger on Seat 
+-- INSERT trigger on Seat
 DECLARE NUMROWS INTEGER;
 BEGIN
     /* erwin Builtin Trigger */
     /* Customer Book Seat on child insert set null */
     /* ERWIN_RELATION:CHECKSUM="0001e839", PARENT_OWNER="", PARENT_TABLE="Customer"
     CHILD_OWNER="", CHILD_TABLE="Seat"
-    P2C_VERB_PHRASE="Book", C2P_VERB_PHRASE="", 
+    P2C_VERB_PHRASE="Book", C2P_VERB_PHRASE="",
     FK_CONSTRAINT="R_12", FK_COLUMNS="Customer_id" */
     UPDATE Seat
     SET
@@ -925,7 +943,7 @@ BEGIN
     /* Flight Have Seat on child insert restrict */
     /* ERWIN_RELATION:CHECKSUM="00000000", PARENT_OWNER="", PARENT_TABLE="Flight"
     CHILD_OWNER="", CHILD_TABLE="Seat"
-    P2C_VERB_PHRASE="Have", C2P_VERB_PHRASE="", 
+    P2C_VERB_PHRASE="Have", C2P_VERB_PHRASE="",
     FK_CONSTRAINT="R_11", FK_COLUMNS="Flight_id" */
     SELECT count(*) INTO NUMROWS
     FROM Flight
@@ -951,14 +969,14 @@ END;
 
 CREATE  TRIGGER tU_Seat AFTER UPDATE ON Seat for each row
     -- erwin Builtin Trigger
--- UPDATE trigger on Seat 
+-- UPDATE trigger on Seat
 DECLARE NUMROWS INTEGER;
 BEGIN
     /* erwin Builtin Trigger */
     /* Customer Book Seat on child update set null */
     /* ERWIN_RELATION:CHECKSUM="00017de2", PARENT_OWNER="", PARENT_TABLE="Customer"
     CHILD_OWNER="", CHILD_TABLE="Seat"
-    P2C_VERB_PHRASE="Book", C2P_VERB_PHRASE="", 
+    P2C_VERB_PHRASE="Book", C2P_VERB_PHRASE="",
     FK_CONSTRAINT="R_12", FK_COLUMNS="Customer_id" */
     /* Not Implemented due to an ORA-04091 Mutating Table Issue */
     NULL;
@@ -994,14 +1012,14 @@ END;
 
 CREATE  TRIGGER  tD_Tour AFTER DELETE ON Tour for each row
     -- erwin Builtin Trigger
--- DELETE trigger on Tour 
+-- DELETE trigger on Tour
 DECLARE NUMROWS INTEGER;
 BEGIN
     /* erwin Builtin Trigger */
     /* Tour  TourEnrollment on parent delete restrict */
     /* ERWIN_RELATION:CHECKSUM="0000e75f", PARENT_OWNER="", PARENT_TABLE="Tour"
     CHILD_OWNER="", CHILD_TABLE="TourEnrollment"
-    P2C_VERB_PHRASE="", C2P_VERB_PHRASE="", 
+    P2C_VERB_PHRASE="", C2P_VERB_PHRASE="",
     FK_CONSTRAINT="R_21", FK_COLUMNS="Tour_id" */
     SELECT count(*) INTO NUMROWS
     FROM TourEnrollment
@@ -1023,14 +1041,14 @@ END;
 
 CREATE  TRIGGER tI_Tour BEFORE INSERT ON Tour for each row
     -- erwin Builtin Trigger
--- INSERT trigger on Tour 
+-- INSERT trigger on Tour
 DECLARE NUMROWS INTEGER;
 BEGIN
     /* erwin Builtin Trigger */
     /* Country  Tour on child insert restrict */
     /* ERWIN_RELATION:CHECKSUM="0000e3de", PARENT_OWNER="", PARENT_TABLE="Country"
     CHILD_OWNER="", CHILD_TABLE="Tour"
-    P2C_VERB_PHRASE="", C2P_VERB_PHRASE="", 
+    P2C_VERB_PHRASE="", C2P_VERB_PHRASE="",
     FK_CONSTRAINT="R_16", FK_COLUMNS="Country_id" */
     SELECT count(*) INTO NUMROWS
     FROM Country
@@ -1056,7 +1074,7 @@ END;
 
 CREATE  TRIGGER tU_Tour AFTER UPDATE ON Tour for each row
     -- erwin Builtin Trigger
--- UPDATE trigger on Tour 
+-- UPDATE trigger on Tour
 DECLARE NUMROWS INTEGER;
 BEGIN
     /* erwin Builtin Trigger */
@@ -1114,14 +1132,14 @@ END;
 
 CREATE  TRIGGER tI_TourEnrollment BEFORE INSERT ON TourEnrollment for each row
     -- erwin Builtin Trigger
--- INSERT trigger on TourEnrollment 
+-- INSERT trigger on TourEnrollment
 DECLARE NUMROWS INTEGER;
 BEGIN
     /* erwin Builtin Trigger */
     /* Tour  TourEnrollment on child insert restrict */
     /* ERWIN_RELATION:CHECKSUM="0001e3f4", PARENT_OWNER="", PARENT_TABLE="Tour"
     CHILD_OWNER="", CHILD_TABLE="TourEnrollment"
-    P2C_VERB_PHRASE="", C2P_VERB_PHRASE="", 
+    P2C_VERB_PHRASE="", C2P_VERB_PHRASE="",
     FK_CONSTRAINT="R_21", FK_COLUMNS="Tour_id" */
     SELECT count(*) INTO NUMROWS
     FROM Tour
@@ -1144,7 +1162,7 @@ BEGIN
     /* Customer  TourEnrollment on child insert restrict */
     /* ERWIN_RELATION:CHECKSUM="00000000", PARENT_OWNER="", PARENT_TABLE="Customer"
     CHILD_OWNER="", CHILD_TABLE="TourEnrollment"
-    P2C_VERB_PHRASE="", C2P_VERB_PHRASE="", 
+    P2C_VERB_PHRASE="", C2P_VERB_PHRASE="",
     FK_CONSTRAINT="R_20", FK_COLUMNS="Customer_id" */
     SELECT count(*) INTO NUMROWS
     FROM Customer
@@ -1170,7 +1188,7 @@ END;
 
 CREATE  TRIGGER tU_TourEnrollment AFTER UPDATE ON TourEnrollment for each row
     -- erwin Builtin Trigger
--- UPDATE trigger on TourEnrollment 
+-- UPDATE trigger on TourEnrollment
 DECLARE NUMROWS INTEGER;
 BEGIN
     /* erwin Builtin Trigger */
